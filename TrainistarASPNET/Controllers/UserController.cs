3d6b6cd5c9ce567f444c9b6e00909ab9f0eb4ddc
hanvt <tronghan05062000@gmail.com>
2023-01-06 14:47:27 +0700

    test

--- TrainistarASPNET/Controllers/UserController.cs
+++ TrainistarASPNET/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System;
 using System.Data;
+using System.Security.Cryptography;
+using System.Text;
 using TrainistarASPNET.Models;
 
 namespace TrainistarASPNET.Controllers
@@ -13,10 +15,28 @@ namespace TrainistarASPNET.Controllers
     public class UserController : ControllerBase
     {
         BaseResponse response = new BaseResponse();
+
         private readonly IConfiguration _configuration;
         public UserController(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        private String toSHA256(String text)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
 
         public String getNextUserId()
@@ -144,7 +164,7 @@ namespace TrainistarASPNET.Controllers
                     using (MySqlCommand cmd = new MySqlCommand(query, con))
                     {
                         cmd.Parameters.AddWithValue("@username", user.username);
-                        cmd.Parameters.AddWithValue("@password", user.password);
+                        cmd.Parameters.AddWithValue("@password", toSHA256(user.password));
                         reader = cmd.ExecuteReader();
                         table.Load(reader);
                         response.role = table.Rows[0][8].ToString();
@@ -187,7 +207,7 @@ namespace TrainistarASPNET.Controllers
                     {
                         cmd.Parameters.AddWithValue("@iduser", user.idUser);
                         cmd.Parameters.AddWithValue("@username", user.username);
-                        cmd.Parameters.AddWithValue("@password", user.password);
+                        cmd.Parameters.AddWithValue("@password", toSHA256(user.password));
                         cmd.Parameters.AddWithValue("@firstname", user.firstName);
                         cmd.Parameters.AddWithValue("@lastname", user.lastName);
                         cmd.Parameters.AddWithValue("@email", user.email);
@@ -237,7 +257,7 @@ namespace TrainistarASPNET.Controllers
 
                 using (MySqlCommand cmd = new MySqlCommand(query, con))
                 {
-                    cmd.Parameters.AddWithValue("@password", user.password);
+                    cmd.Parameters.AddWithValue("@password", toSHA256(user.password));
                     cmd.Parameters.AddWithValue("@firstname", user.firstName);
                     cmd.Parameters.AddWithValue("@lastname", user.lastName);
                     cmd.Parameters.AddWithValue("@email", user.email);
